4046d1fab1f9640c7b6b1f4ad6bd45a57151666a
unknown <you@example.com>
2021-08-04 11:19:53 +0800

    更新设置信息 和展示状态

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,84 +1,128 @@
 <template>
   <div id="settingIndex">
     <Tyh-card shadow="always" simple>
-      <h3 class="title">账号信息设置</h3>
+      <h3 class="title">
+        {{ changeUserInfoBloon ? "修改资料" : "账号信息设置" }}
+      </h3>
       <div class="user_photo_box">
-        <img class="user_photo" :src="userInfo.photo" alt="" />
+        <img
+          class="user_photo"
+          :src="userInfo.photo"
+          alt="用户头像"
+          @click="upFilePhoto"
+        />
+        <input ref="file_input" class="up_user_photo" type="file" />
       </div>
 
-      <el-form :model="userForm" ref="userForm" label-width="70px">
+      <el-form
+        :model="userForm"
+        ref="userForm"
+        label-width="80px"
+        :rules="userFormRules"
+      >
         <!-- 昵称 -->
-        <el-form-item label="昵称">
+        <el-form-item label="昵称" prop="nickname">
           <el-input
+            v-if="changeUserInfoBloon"
             v-model="userForm.nickname"
             clearable
             placeholder="编辑昵称"
           />
+          <p v-else class="form_item_text">{{ userForm.nickname }}</p>
         </el-form-item>
 
         <!-- 个性签名 -->
         <el-form-item label="个性签名">
           <el-input
+            v-if="changeUserInfoBloon"
             v-model="userForm.autograph"
             clearable
             placeholder="选择个性签名"
           />
+          <p v-else class="form_item_text">{{ userForm.autograph }}</p>
         </el-form-item>
 
         <!-- 性别 -->
         <el-form-item label="性别">
-          <el-select v-model="userForm.gender" placeholder="请选择性别">
-            <el-option label="男" value="1" />
-            <el-option label="女" value="2" />
-            <el-option label="保密" value="3" />
+          <el-select
+            v-if="changeUserInfoBloon"
+            v-model="userForm.gender"
+            placeholder="请选择性别"
+          >
+            <el-option value="男" />
+            <el-option value="女" />
+            <el-option value="保密" />
           </el-select>
+          <p v-else class="form_item_text">{{ userForm.gender }}</p>
         </el-form-item>
 
         <!-- 感情状况 -->
         <el-form-item label="感情状况">
-          <el-select v-model="userForm.feeling" placeholder="感情状况">
+          <el-select
+            v-if="changeUserInfoBloon"
+            v-model="userForm.feeling"
+            placeholder="感情状况"
+          >
             <el-option
               v-for="(feelingItem, index) in feelingList"
               :key="index"
-              :label="feelingItem"
-              :value="index"
+              :value="feelingItem"
             />
           </el-select>
+          <p v-else class="form_item_text">{{ userForm.feeling }}</p>
         </el-form-item>
 
         <!-- 职业 -->
         <el-form-item label="职业">
-          <el-select v-model="userForm.work" placeholder="选择职业">
+          <el-select
+            v-if="changeUserInfoBloon"
+            v-model="userForm.work"
+            placeholder="选择职业"
+          >
             <el-option
               v-for="(workItem, index) in workList"
               :key="index"
-              :label="workItem"
-              :value="index"
+              :value="workItem"
             />
           </el-select>
+          <p v-else class="form_item_text">{{ userForm.work }}</p>
         </el-form-item>
 
         <!-- 生日 -->
         <el-form-item label="生日">
           <el-date-picker
+            v-if="changeUserInfoBloon"
             v-model="userForm.birthday"
             type="date"
             placeholder="选择日期"
+            format="yyyy 年 MM 月 dd 日"
           />
+          <p v-else class="form_item_text">{{ userForm.birthday }}</p>
         </el-form-item>
 
         <!-- 邮箱 -->
-        <el-form-item label="邮箱">
-          <el-input v-model="userForm.mail" clearable placeholder="选择邮箱" />
+        <el-form-item label="邮箱" prop="mail">
+          <el-input
+            v-if="changeUserInfoBloon"
+            v-model="userForm.mail"
+            clearable
+            placeholder="选择邮箱"
+          />
+          <p v-else class="form_item_text">{{ userForm.mail }}</p>
         </el-form-item>
       </el-form>
 
-      <Tyh-button type="success">保存资料</Tyh-button>
+      <Tyh-button
+        :type="changeUserInfoBloon ? 'success' : 'primary'"
+        :simple="changeUserInfoBtnSimple"
+        @click="changeUserInfoBloon ? SaveData() : (changeUserInfoBloon = true)"
+        >{{ changeUserInfoBloon ? "保存资料" : "修改资料" }}</Tyh-button
+      >
     </Tyh-card>
 
     <Tyh-card shadow="always" simple>
       <h3 class="title">修改密码</h3>
-      <el-form :model="changePass" ref="changePass" label-width="70px">
+      <el-form :model="changePass" ref="changePass" label-width="80px">
         <!-- 原始密码 -->
         <el-form-item label="原始密码">
           <el-input
@@ -120,7 +164,7 @@
 </template>
 
 <script>
-import { getUserInfo } from '@/api/user'
+import { getUserInfo, changeUserInfo } from '@/api/user'
 import { mapState } from 'vuex'
 export default {
   name: 'settingIndex',
@@ -128,8 +172,10 @@ export default {
   props: {},
   data () {
     return {
+      changeUserInfoBloon: false, // 展示用户信息和编辑信息的切换状态
+      changeUserInfoBtnSimple: false, // 更新用户信息的按钮禁用状态
       feelingList: ['单身', '已婚', '订婚', '暧昧中', '求交往', '暗恋中', '分居', '离异', '保密'], // 感觉状况
-      workList: ['计算机/互联网/通信', '生产/工艺/制造', '金融/银行/投资/保险', '商业/服务业/个体经营', '文化/广告/传媒', '娱乐/艺术/表演', '律师/法务', '教育/培训', '公务员/行政/事业单位', '演员/歌手', '模特', '空姐', '学生', '自由职业', '其他'], // 工作列表
+      workList: ['计算机/互联网/通信', '生产/工艺/制造', '金融/银行/投资/保险', '商业/服务业/个体经营', '文化/广告/传媒', '娱乐/艺术/表演', '律师/法务', '教育/培训', '公务员/行政/事业单位', '演员/歌手', '自由职业', '模特', '空姐', '学生', '其他'], // 工作列表
       userForm: {
         nickname: '', // 昵称
         autograph: '', // 个性签名
@@ -144,6 +190,19 @@ export default {
         oldPass: '',
         newPass1: '',
         newPass2: ''
+      },
+      // 用户资料的表单验证
+      userFormRules: {
+        // 昵称
+        nickname: [
+          { required: true, message: '请输入昵称', trigger: 'blur' },
+          { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+        ],
+        // 邮箱
+        mail: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { pattern: /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/, message: '邮箱格式不正确', trigger: 'blur' }
+        ]
       }
     }
   },
@@ -159,7 +218,7 @@ export default {
     // 获取用户信息
     async loadgetUserInfo () {
       const { data } = await getUserInfo(this.userInfo.id)
-      this.userForm = data
+      this.userForm = data.data
     },
     // 退出登录
     outLogin () {
@@ -177,6 +236,36 @@ export default {
         })
         this.$router.push('/')
       }).catch(() => { })
+    },
+    // 点击头像上传文件
+    upFilePhoto () {
+      const fileInput = this.$refs.file_input
+      fileInput.click()
+    },
+    // 保存用户资料
+    SaveData () {
+      this.changeUserInfoBtnSimple = true
+      this.$refs.userForm.validate(async (valid) => {
+        if (!valid) {
+          this.changeUserInfoBtnSimple = false
+          return
+        }
+        const newObj = {}
+        newObj.id = this.userInfo.id
+        for (const key in this.userForm) {
+          newObj[key] = this.userForm[key]
+        }
+        const { data } = await changeUserInfo(this.$qs.stringify(newObj))
+        // 改变展示状态和按钮禁用状态
+        this.changeUserInfoBloon = false
+        this.changeUserInfoBtnSimple = false
+        this.$message({
+          message: '更新用户信息成功',
+          type: 'danger',
+          iconClass: 'tyh-ui-success-01'
+        })
+        console.log(data)
+      })
     }
   }
 }
@@ -201,17 +290,24 @@ export default {
         border-radius: 50%;
         cursor: pointer;
       }
+      .up_user_photo {
+        display: none;
+      }
     }
     .el-form {
       margin-top: 25px;
+      .form_item_text {
+        color: #333;
+        margin-left: 13px;
+      }
     }
     .el-select,
     .el-date-editor--date {
       width: 100%;
     }
     .tyh-button {
-      width: 540px;
-      margin-left: 70px;
+      width: 530px;
+      margin-left: 80px;
       margin-bottom: 16px;
     }
   }
